8349f12f0a333d20f3cb7c2728e09392ce607cf5
yosu <woodstock830@gmail.com>
2025-09-27 12:45:11 +0900

    Change mailer from SendGrid to Mailtrap

--- config/runtime.exs
+++ config/runtime.exs
@@ -116,6 +116,5 @@ if config_env() == :prod do
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
   config :dahlia, Dahlia.Mailer,
     adapter: Swoosh.Adapters.Sendgrid,
-    api_key: System.get_env("SENDGRID_API_KEY"),
-    compress: true
+    api_key: System.get_env("MAILTRAP_API_KEY")
 end
